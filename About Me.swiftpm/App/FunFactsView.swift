3d91435ec282131ee13d13194c9d4fb5999549fb
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-12-27 23:18:25 +0900

    first commit

---
+++ About Me.swiftpm/App/FunFactsView.swift
@@ -0,0 +1,42 @@
+/*
+See the License.txt file for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+struct FunFactsView: View {
+
+    @State private var funFact = ""
+    var body: some View {
+        VStack {
+            Text("Fun Facts")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                        
+            Text(funFact)
+                .padding()
+                .font(.title)
+                .frame(minHeight: 400)
+
+            Button("Show Random Fact") {
+                funFact = information.funFacts.randomElement()!
+            }
+            .padding()
+            .background(Color.cyan)
+            .foregroundColor(Color.indigo)
+            .cornerRadius(15)
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.indigo, lineWidth: 2)
+            )
+
+        }
+        .padding()
+    }
+}
+
+struct FunFactsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FunFactsView()
+    }
+}
